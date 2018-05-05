786a324cf2a197e6dba3e5fbafa89f145cfa6b84
Chris Chen <cschen13@gmail.com>
2018-05-05 14:47:02 -0500

    one new gesture

---
+++ gestures.js
@@ -0,0 +1,189 @@
+const gestures = [
+  [
+  {
+    "name": "Pinky finger",
+    "bones": [
+      {
+        "name": "Metacarpal",
+        "direction": [
+          0.426036,
+          0.0453472,
+          -0.903569
+        ]
+      },
+      {
+        "name": "Proximal phalanx",
+        "direction": [
+          0.314493,
+          -0.121209,
+          -0.94149
+        ]
+      },
+      {
+        "name": "Intermediate phalanx",
+        "direction": [
+          0.194419,
+          -0.178063,
+          -0.964622
+        ]
+      },
+      {
+        "name": "Distal phalanx",
+        "direction": [
+          0.101448,
+          -0.218834,
+          -0.970474
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Thumb",
+    "bones": [
+      {
+        "name": "Metacarpal",
+        "direction": [
+          -0.0691118,
+          0.544374,
+          -0.835991
+        ]
+      },
+      {
+        "name": "Proximal phalanx",
+        "direction": [
+          -0.578786,
+          0.443512,
+          -0.684327
+        ]
+      },
+      {
+        "name": "Intermediate phalanx",
+        "direction": [
+          -0.36662,
+          0.126342,
+          -0.921752
+        ]
+      },
+      {
+        "name": "Distal phalanx",
+        "direction": [
+          -0.49015,
+          0.299965,
+          -0.818397
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Index finger",
+    "bones": [
+      {
+        "name": "Metacarpal",
+        "direction": [
+          0.078362,
+          0.289375,
+          -0.954003
+        ]
+      },
+      {
+        "name": "Proximal phalanx",
+        "direction": [
+          0.428294,
+          0.291819,
+          -0.855223
+        ]
+      },
+      {
+        "name": "Intermediate phalanx",
+        "direction": [
+          0.213165,
+          0.00365758,
+          -0.977009
+        ]
+      },
+      {
+        "name": "Distal phalanx",
+        "direction": [
+          0.00195563,
+          -0.246527,
+          -0.969134
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Middle finger",
+    "bones": [
+      {
+        "name": "Metacarpal",
+        "direction": [
+          0.187748,
+          0.206792,
+          -0.960202
+        ]
+      },
+      {
+        "name": "Proximal phalanx",
+        "direction": [
+          0.489693,
+          0.147292,
+          -0.859364
+        ]
+      },
+      {
+        "name": "Intermediate phalanx",
+        "direction": [
+          0.202819,
+          -0.116383,
+          -0.972275
+        ]
+      },
+      {
+        "name": "Distal phalanx",
+        "direction": [
+          -0.0703736,
+          -0.327854,
+          -0.942104
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Ring finger",
+    "bones": [
+      {
+        "name": "Metacarpal",
+        "direction": [
+          0.302046,
+          0.108994,
+          -0.947042
+        ]
+      },
+      {
+        "name": "Proximal phalanx",
+        "direction": [
+          0.285149,
+          -0.0546534,
+          -0.956924
+        ]
+      },
+      {
+        "name": "Intermediate phalanx",
+        "direction": [
+          0.20194,
+          -0.115418,
+          -0.972573
+        ]
+      },
+      {
+        "name": "Distal phalanx",
+        "direction": [
+          0.136203,
+          -0.161472,
+          -0.977433
+        ]
+      }
+    ]
+  }
+]
+];
